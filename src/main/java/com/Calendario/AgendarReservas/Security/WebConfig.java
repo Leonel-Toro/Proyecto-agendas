2a0ef91348fdd038395564b212481d028c79ddd0
Leonel Toro <128005116+Leonel-Toro@users.noreply.github.com>
2025-12-10 21:40:22 -0300

    se prepara la app para el deploy

--- src/main/java/com/Calendario/AgendarReservas/Security/WebConfig.java
+++ src/main/java/com/Calendario/AgendarReservas/Security/WebConfig.java
@@ -1,7 +1,6 @@
 package com.Calendario.AgendarReservas.Security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,14 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private String allowedOriginsString;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // Split por coma para soportar múltiples orígenes
+        String[] allowedOrigins = allowedOriginsString.split(",");
+
         registry.addMapping("/api/**")
                 .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET","POST","PUT","OPTIONS")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600); // cachea preflight 1 hora
     }
 }
